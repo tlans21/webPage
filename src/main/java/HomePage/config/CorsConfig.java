8b5ce509875bc91328a74ca3941e9239c4812466
tlans21 <cjstlans21@naver.com>
2024-03-07 22:25:41 +0900

    세션 방식 -> jwt 로그인 방식으로 변경하기 위해 시큐리티 필터 체인 옵션 변경

---
+++ src/main/java/HomePage/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package HomePage.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+
+}
--- src/main/java/HomePage/config/SecurityConfig.java
+++ src/main/java/HomePage/config/SecurityConfig.java
@@ -2,14 +2,15 @@ package HomePage.config;
 
 
 import HomePage.config.oauth.PrincipalOauth2UserService;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) //secured 어노테이션 활성화, preAuthorization 활성화
@@ -17,36 +18,62 @@ public class SecurityConfig{
     @Autowired
     private PrincipalOauth2UserService principalOauth2UserService;
 
+    @Autowired
+    private CorsFilter corsFilter;
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Session 방식 로그인
+//       http
+//                .csrf((csrfConfig) ->
+//                        csrfConfig.disable()
+//                )
+//                .authorizeHttpRequests((authorizeRequests) ->
+//                        authorizeRequests
+//                                .requestMatchers("/user/**").authenticated()
+//                                .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+//                                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .anyRequest().permitAll()
+//                )
+//                .formLogin((formLogin) ->
+//                        formLogin
+//                                .loginPage("/loginForm")
+//                                .loginProcessingUrl("/login")
+//                                .defaultSuccessUrl("/index")
+//                )
+//                .oauth2Login((oauth2Login) ->
+//                            oauth2Login
+//                                    .loginPage("/loginForm")
+//                                    .defaultSuccessUrl("/")
+//                                    .failureUrl("/loginForm")
+//                                    .userInfoEndpoint((userInfoEndpoint) ->
+//                                            userInfoEndpoint
+//                                                    .userService(principalOauth2UserService)
+//                                    )
+//                );
+//
+//        return http.build();
+        // JWT 방식
         http
-                .csrf((csrfCConfig) ->
-                        csrfCConfig.disable()
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                .sessionManagement((sessionManagementConfig) -> sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilter(corsFilter)
+                .formLogin((formLogin) -> formLogin.disable()
+                )
+                .httpBasic((httpBasicConfig) -> httpBasicConfig.disable()
                 )
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
-                                .requestMatchers("/user/**").authenticated()
-                                .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                                .requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
+                                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                 .anyRequest().permitAll()
-                )
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/loginForm")
-                                .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/index")
-                )
-                .oauth2Login((oauth2Login) ->
-                            oauth2Login
-                                    .loginPage("/loginForm")
-                                    .defaultSuccessUrl("/")
-                                    .failureUrl("/loginForm")
-                                    .userInfoEndpoint((userInfoEndpoint) ->
-                                            userInfoEndpoint
-                                                    .userService(principalOauth2UserService)
-                                    )
                 );
-
         return http.build();
     }
 }
