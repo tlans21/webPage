e5e401b6aeb8983f6faf21884265909616348a45
tlans21 <cjstlans21@naver.com>
2024-12-10 18:22:07 +0900

    [test] : 부하 테스트를 위한 login sendRedirect 주석 처리  - 웹브라우저 상에선 로그인 성공후 인덱스 페이지로 리다이렉트후 cookie가 정상적으로 등록됨  - k6로 요청시 리다이렉트 되어 쿠키가 유실되는 것을 확인  - 따라서 부하테스트 중에는 리다이렉트 제거

---
+++ src/main/java/HomePage/service/NavbarService.java
@@ -0,0 +1,13 @@
+package HomePage.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NavbarService {
+//    private final CacheManager cacheManager;
+//    private static final String NAV_CACHE_NAME = "navBarCache";
+//
+
+}
